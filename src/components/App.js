8622887200923733074cae53a8ffda04f937ca59
jayson <jayson@penguin>
2022-01-24 17:39:01 -0500

    Setup Firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,17 @@
 import './App.css';
 
+import firebase from 'firebase/app';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDPaFClETBTNgX_NSF2dmLFN8vtW96VmBM",
+    authDomain: "jaysonc-dev.firebaseapp.com",
+    projectId: "jaysonc-dev",
+    storageBucket: "jaysonc-dev.appspot.com",
+    messagingSenderId: "53708482599",
+    appId: "1:53708482599:web:d16c45184aa5d05005c602",
+    measurementId: "G-BMPERF6LEH"
+});
+
 function App() {
     return (
         <div className="App">
